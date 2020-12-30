08d1b97f936a77e49a8620f34d6e47f35351c0dd
Roger Jiang <imrogerjiang@gmail.com>
2020-12-31 10:45:03 +1100

    created dynamically generated portfolio page

--- app/load_json.py
+++ app/load_json.py
@@ -3,34 +3,49 @@ import os
 from datetime import date
 from flask import Markup
 
-# Load all json from content/posts into list
-all_posts = []
+class Assets():
+    def __init__(self, src=None):
+        self.src = src
 
-for filename in os.listdir("app/static/assets/posts/"):
-    if filename.find(".json") > -1:
-        with open("app/static/assets/posts/" + filename) as f:
-            all_posts.append(json.load(f))
 
-# Post processing
-for post in all_posts:
-    post["date"] = date.fromisoformat(post["date"])
-    post["body"] = Markup(post["body"])
+    # TODO: Implement checking of file address here catch/raise
+    def load_asset(self, processing=None):
+        self.all_assets = []
 
-# Returns n most recent posts
-def load_recent(n=10, start=0):
-    if start > len(all_posts):
-        raise ValueError("Start argument larger than number of posts.")
+        for filename in os.listdir(self.src):
+            if filename.find(".json") > -1:
+                with open(self.src + filename) as f:
+                    self.all_assets.append(json.load(f))
+
+        processing(self.all_assets)
+
+    def load_recent(self, n=10, start=0):
+        if self.all_assets is None:
+            raise AttributeError("Json assets has not yet been loaded.")
+
+        if start > len(self.all_assets):
+            raise ValueError("Start argument larger than number of posts.")
+        
+        start = min(len(self.all_assets), start)
+        n = min(len(self.all_assets)-start, n)
+        self.all_assets.sort(key=lambda x:x["date"], reverse=True)
+        return self.all_assets[start:start+n]
+
+
     
-    start = min(len(all_posts), start)
-    n = min(len(all_posts)-start, n)
-    all_posts.sort(key=lambda x:x["date"], reverse=True)
-    return all_posts[start:start+n]
-
-
-
 if __name__ == "__main__":
     from pprint import pprint
 
-    pprint(load_recent(10))
+    def portfolio_processing(portfolio):
+        for sample in portfolio:
+            sample["date"] = date.fromisoformat(sample["date"])
+            sample["body"] = Markup(sample["body"])
+
+    portfolio = Assets(src="app/static/assets/portfolio/")
+    portfolio.load_asset(processing=portfolio_processing)
+    pprint(portfolio.load_recent(10))
+
+
+
 
 
--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,27 @@
 # flask run
 
 from app import app
-from .load_json import load_recent
+from datetime import date
+from .load_json import Assets
 from flask import render_template
 from flask import Markup
+
+
+# TODO: USE DB instead of json
+def posts_processing(posts):
+    for post in posts:
+        post["date"] = date.fromisoformat(post["date"])
+        post["body"] = Markup(post["body"])
+posts = Assets(src="app/static/assets/posts/")
+posts.load_asset(processing=posts_processing)
+
+def portfolio_processing(portfolio_samples):
+    for sample in portfolio_samples:
+        sample["date"] = date.fromisoformat(sample["date"])
+        sample["body"] = Markup(sample["body"])
+portfolio_samples = Assets(src="app/static/assets/portfolio/")
+portfolio_samples.load_asset(processing=portfolio_processing)
+
 
 @app.route('/')
 @app.route('/index')
@@ -16,11 +34,11 @@ def index():
 app.add_template_global(lambda x: x.strftime("%Y %b %-d"), name="blog_date")
 @app.route('/blog')
 def blog():
-    return render_template("blog.html" ,title="Blog", posts=load_recent(10))
+    return render_template("blog.html" ,title="Blog", posts=posts.load_recent(10))
 
 @app.route('/portfolio')
 def portfolio():
-    return "Portfolio!~"
+    return render_template("portfolio.html" ,title="Portfolio", samples=portfolio_samples.load_recent(10))
 
 @app.route('/about')
 def about():
